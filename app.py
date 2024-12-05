d60eeec8ad39f68519001f1a4e21396566f1b54f
Vikrant Verma <vikrant.verma0210@gmail.com>
2024-12-04 19:03:08 -0800

    username entry, tests to search for user in db

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def signup():
     if request.method == "POST":
         first_name = request.form.get("fname")
         last_name = request.form.get("lname") 
-        DBInterface.writeUser(first_name, last_name)
+        username = request.form.get("username") 
+
+        DBInterface.writeUser(first_name, last_name, username)
         
     return render_template('create_user_form.html')
-    
---
+++ tests/test_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+import pytest
+
+conn = sqlite3.connect('./data/db.db')
+
+def test_username_search(username = "flaskusser"):
+    cursor = conn.cursor() 
+    cursor.execute("SELECT 1 FROM USERS WHERE (Username == '" + username + "')")
+    rows = cursor.fetchall()
+    
+    assert rows
+    
+def test_user_not_in_db(username = "bro"):
+    cursor = conn.cursor() 
+    cursor.execute("SELECT 1 FROM USERS WHERE (Username == '" + username + "')")
+    rows = cursor.fetchall()
+    
+    assert not rows

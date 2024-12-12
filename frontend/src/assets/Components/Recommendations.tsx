71c98458e8aaa672947063611179f62f5cea95df
GutsyShinobi7 <109954442+GutsyShinobi7@users.noreply.github.com>
2024-12-11 18:29:30 -0800

    fixing this crazy ahhh api key mannn

--- frontend/src/assets/Components/Recommendations.tsx
+++ frontend/src/assets/Components/Recommendations.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Fuse from "fuse.js";
-// import * as dotenv from "dotenv";
 
-// dotenv.config();
 interface RecommendationsProps {
   username: string;
 }
